f5eb461e2038b1eaf34b9c6dfec55229b510f983
anubbhhavraj <anubhavraj2808@gmail.com>
2024-09-16 03:26:35 +0530

    change

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,22 @@ const authController = require('../controllers/authController');
 
 
 router.get('/', (req, res) => {
-    res.render('index'); // This will render the index.ejs file in your views directory
+    res.render('index'); 
+  });
+  router.post('/auth/instagram/deauthorize', (req, res) => {
+     
+ 
+    console.log('User deauthorized:', req.body);
+    res.sendStatus(200);  
+  });
+  router.post('/auth/instagram/delete', (req, res) => {
+   
+    console.log('Data deletion request received:', req.body);
+   
+    res.json({
+      url: 'https://crushfinder-gtfz.onrender.com/privacy-policy',  
+      confirmation_code: 'unique_confirmation_code'  
+    });
   });
 router.get('/auth/instagram', authController.authInstagram);
 router.get('/auth/instagram/callback', authController.authInstagramCallback);

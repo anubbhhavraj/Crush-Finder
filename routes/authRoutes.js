ac8e07c51c7a9f49ff5b3d2371349d0992b411a0
anubbhhavraj <anubhavraj2808@gmail.com>
2024-09-16 03:07:31 +0530

    index

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+
+
+router.get('/', (req, res) => {
+    res.render('index'); // This will render the index.ejs file in your views directory
+  });
 router.get('/auth/instagram', authController.authInstagram);
 router.get('/auth/instagram/callback', authController.authInstagramCallback);
 
